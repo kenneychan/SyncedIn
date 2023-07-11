ab179c866f7639bbdf71815978ca99ec297c2763
Kenney Chan <kchan10@gmail.com>
2023-07-11 14:58:28 -0700

    Add roles to users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
+// create controller module
+const usersCtrl = require("../controller/users");
+
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", usersCtrl.index);
+
+router.post("/:id", usersCtrl.update);
 
 module.exports = router;
