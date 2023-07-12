a9cc8af2c8b88e21234e1b0eb37074ae2dbdab12
Samuel Baird <sam@samuelbaird.com>
2023-07-12 16:49:13 -0300

    About section edit functionality, and hidden form

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const bioEditButton = document.getElementById("bioEditButton");
+const bioEditForm = document.getElementById("bioEditForm");
+
+bioEditButton.addEventListener("click", function () {
+  bioEditForm.style.display = "block";
+});
