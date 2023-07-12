4713610403c90636337b45893cbdda1c097a5889
Alex Taylor <alextaylor@Fannys-MacBook-Pro-2.local>
2023-07-12 08:39:43 -0700

    added index and new page

---
+++ controllers/jobs.js
@@ -0,0 +1,21 @@
+var Job = require('../models/user');
+
+module.exports = {
+    index, 
+    new: newJob,
+    create
+};
+
+async function index(req, res) {
+    const jobs = await Job.find({});
+    res.render('jobs/index', { title: 'All Jobs', jobs });
+  }
+
+  function newJob(req, res) {
+    res.render('jobs/new', {title: 'Add New Job', errorMsg: ' '});
+  }
+  
+  async function create(req, res){
+    await Job.create(req.body);
+    res.redirect('/jobs');
+}
---
+++ routes/jobs.js
@@ -0,0 +1,15 @@
+// In your routes file
+var express = require('express');
+var router = express.Router();
+var ensureLoggedIn = require('../config/ensureLoggedIn');
+var syncCtrl = require('../controllers/jobs');
+var jobs = require('../models/job'); // Import the correct model file
+
+/* GET jobs listing. */
+router.get('/', syncCtrl.index);
+// get new jobs
+router.get('/new', syncCtrl.new);
+// Create job posting
+router.post('/', syncCtrl.create);
+
+module.exports = router;
