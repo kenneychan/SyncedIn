76db4349e41c65c96ff342fc31f3d22cefef0f40
Kenney Chan <kchan10@gmail.com>
2023-07-13 23:42:41 -0700

    Added Bootstrap carousel and user featurettes

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const Job = require("../models/job");
+const User = require("../models/user");
+
+module.exports = {
+  index,
+};
+
+async function index(req, res) {
+  const users = await User.find();
+  const jobs = await Job.findById();
+
+  res.render("index", {
+    title: "SyncedIn",
+    users,
+    jobs,
+  });
+}
